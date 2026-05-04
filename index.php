8e2265cf063dceb397ab87a9bcbe97da24942a5a
Chingupingu30 <teodavid2994@gmail.com>
2026-05-04 15:18:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php require __DIR__ . '/nyw55b.php' ?>
 <!DOCTYPE html>
 <html lang="en">
    <head>
+         <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5024631,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5024631&101" alt="free hit counters" border="0"></a></noscript>
+<!-- Histats.com  END  -->
       <!-- basic -->
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
